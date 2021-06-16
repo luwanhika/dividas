fdc1afd8609483af50cf4a776d11043ceb600268
luwanhika <luwanhika@hotmail.com>
2021-06-16 20:47:51 -0300

    ajustes finais

--- src/components/templete/Footer.jsx
+++ src/components/templete/Footer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default props => 
   <footer className="footer">
-    Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+    Desenvolvido <i className="text-danger"></i> por
     <strong> Luwa<span className="text-danger">nhi</span>ka</strong>
   </footer>
--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -7,37 +7,50 @@ const headerProps = {
   subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
-
-
 export default function UserCrud() {
 
-  const[id, setId] = useState('');
+  const [id, setId] = useState('');
+  const [users, setUsers] = useState([]);
+  const [dividas, setDividas] = useState([]);
+  const [desc_divida, setDescDivida] = useState('');
+  const [valor_divida, setValorDivida] = useState('');
+  const [editing, setEditing] = useState(false);
+  const [idDivida, setIdDivida] = useState('');
 
-  const[users, setUsers] = useState([]);
-  const[dividas, setDividas] = useState([]);
+ async function getUserId(id) {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+    return response.data.name;
+  }
 
-  const[desc_divida, setDescDivida] = useState('');
-  const[valor_divida, setValorDivida] = useState('');
+  const loadDivida = async () => {
+    const response = await axios.get('https://provadev.xlab.digital/api/v1/divida?uuid=bad2fe24-5b1d-44e2-93e5-a7c958b414d5')
+    const { result: dividas } = response.data;
 
-  const[editing, setEditing] = useState(false);
-  const[userEditing, setUserEditing] = useState([])
-  const [idDivida, setIdDivida] = useState("")
+    const dividasWithUser = await Promise.all(dividas.map(async item => {
+      const username = await getUserId(item.idUsuario);
+      return {
+        ...item,
+        username,
+        valorFormatted: Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(item.valor),
+        criado: new Date(item.criado).toLocaleDateString('pt-BR', 'dd/mm/YYYY')
+      }
+    })
+   );
+    setDividas(dividasWithUser)
+ }
 
+ const loadUsers = async () => {
+   const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+   setUsers(response.data);
+ }
 
  useEffect(() => {
-   axios.get('https://provadev.xlab.digital/api/v1/divida?uuid=bad2fe24-5b1d-44e2-93e5-a7c958b414d5')
-   .then(resp => {
-      setDividas(resp.data.result)
-   })
- }, []);
-
-
- useEffect(() => {
-  axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(resp => {
-      setUsers(resp.data)
-   })
- }, []);
+   loadUsers();
+    loadDivida();
+ },[]);
 
   function salvar() {
     if(editing) {
@@ -45,21 +58,27 @@ export default function UserCrud() {
         idUsuario: id,
         motivo: desc_divida,
         valor: valor_divida
-      }).then(resp => console.log(resp.data));
+      }).then(() => {
+        clear();
+        loadDivida()
+      });
     } else {
       axios.post('https://provadev.xlab.digital/api/v1/divida?uuid=bad2fe24-5b1d-44e2-93e5-a7c958b414d5', 
       {
         idUsuario: id,
         motivo: desc_divida,
         valor: valor_divida
-      }).then(resp => setDividas([ ...dividas, resp.data ]));
+      }).then(resp => {
+        setDividas([ ...dividas, resp.data ]);
+        clear();
+        loadDivida();
+      });
     }
-   
   }
 
-  function del(id) {
+  function remove(id) {
     axios.delete(`https:provadev.xlab.digital/api/v1/divida/${id}?uuid=bad2fe24-5b1d-44e2-93e5-a7c958b414d5`)
-    .then(resp => {
+    .then(() => {
       const dividasFilter = dividas.filter(divida => divida._id !== id)
       setDividas(dividasFilter)
     })
@@ -73,58 +92,13 @@ export default function UserCrud() {
     setValorDivida('')
   }
 
-  function upDate(divida) {
-    setUserEditing(divida)
+  function update(divida) {
     setEditing(true)
     setIdDivida(divida._id)
     setId(divida.idUsuario)
     setDescDivida(divida.motivo)
-    setValorDivida(divida.valor)
+    setValorDivida(divida.valor);
   }
-
-
-//  getDivida() {
-//   axios.get('https://provadev.xlab.digital/api/v1?uuid=bad2fe24-5b1d-44e2-93e5-a7c958b414d5/divida')
-//     .then(resp => {
-//       this.setState({ divida: resp.data })
-//     })
-//  }
-
-
-  // save() {
-  //   const user = this.state.user
-  //   const method = user.id ? 'put' : 'post'
-  //   const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
-  //   axios[method](url, user)
-  //   .then(resp => {
-  //     const list = this.getUpdatedList(resp.data)
-  //     this.setState({ user: initialState.user, list })
-  //   })
-  // }
-
-  // getUpdatedList(user, add = true) {
-  //   const list = this.state.list.filter(u => u.id !== user.id)
-  //   if(add) list.unshift(user)
-  //   return list
-  // }
-
-  // updateField(event) {
-  //   const user = { ...this.state.user }
-  //   user[event.target.name] = event.target.value
-  //   this.setState({ user })
-  // }
-
-  // handleChange(event) {
-  //   this.setState({iduser: event.target.value})
-  // }
-
-  // remove(user) {
-  //   axios.delete(`${baseUrl}/${user.id}`).then(resp => {
-  //     const list = this.getUpdatedList(user, false)
-  //     this.setState({ list })
-  //   })
-  // }
-
 
     return (
       <>
@@ -133,9 +107,12 @@ export default function UserCrud() {
           <div className="col-12 col-md-6">
             <div className="form-group mt-2">
               <label>Cliente</label>
-              <select className="form-control" onChange={e => 
-                setId(e.target.value)} >
-                 { users.map(user => 
+              <select 
+                className="form-control" 
+                onChange={e => setId(e.target.value)} 
+              >
+                 { 
+                  users.map(user => 
                    (
                       <option key={user.id} className="form-control" 
                         name="name"
@@ -144,13 +121,7 @@ export default function UserCrud() {
                       </option>
                     ))
                   }
-                  
               </select>
-              {/* <input type="text" className="form-control"
-                name="name"
-                value={this.state.user.name}
-                onChange={e => this.updateField(e)}
-                placeholder="Usuário do JSONPlaceholder" /> */}
             </div>
           </div>
 
@@ -196,10 +167,10 @@ export default function UserCrud() {
       <table className="table mt-4">
       <thead>
         <tr>
-          <th>ID</th>
           <th>Nome</th>
           <th>Motivo</th>
           <th>Valor</th>
+          <th>Data</th>
           <th>Ações</th>
         </tr>
       </thead>
@@ -208,17 +179,17 @@ export default function UserCrud() {
           dividas.map(user => 
              (
               <tr key={user.id}>
-                <td>{user.idUsuario}</td>
-                <td>{user.nome}</td>
+                <td>{user.username}</td>
                 <td>{user.motivo}</td>
-                <td>{user.valor}</td>
+                <td>{user.valorFormatted}</td>
+                <td>{user.criado}</td>
                 <td>
                   <button className="btn btn-warning m-2"
-                    onClick={() => upDate(user)}>
+                    onClick={() => update(user)}>
                     <i className="fa fa-pencil"></i>
                   </button>
                   <button className="btn btn-danger m-2"
-                    onClick={() => del(user._id)}>
+                    onClick={() => remove(user._id)}>
                     <i className="fa fa-trash"></i>
                   </button>
                 </td>
